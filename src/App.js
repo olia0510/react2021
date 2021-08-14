cac22ae7cdf2f53122370ce49755e2f461f64cd8
olia0510 <olia.viderskaya@gmail.com>
2021-08-14 20:48:45 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-
-
-
-export default function App(){
-    return(
-        <div className='User'>
-            <User name = {'Leanne Graham'} username = {'Bret'} id = { 1} email={'Sincere@april.biz'}  address:street={'Kulas Light'} address:suite={'Apt. 556'} address:city={'Gwenborough'} address:zipcode={'92998-3874'} geo:lat={'-37.3159'} geo:lng={'81.1496'}/>
-            <User name = {'Ervin Howell'} username = {'Antonette'} id = { 2} email={'Shanna@melissa.tv'}  address:street={'Victor Plains'} address:suite={'Suite 879'} address:city={'Wisokyburgh'} address:zipcode={'90566-7771'} geo:lat={'-43.9509'} geo:lng={'-34.4618'}/>
-            <User name = {'Clementine Bauch'} username = {'Samantha'} id = { 3} email={'Nathan@yesenia.net'}  address:street={'Douglas Extension'} address:suite={'Suite 847'} address:city={'McKenziehaven'} address:zipcode={'59590-4157'} geo:lat={'-68.6102'} geo:lng={'-47.0653'}/>
-            <User name = {'Patricia Lebsack'} username = {'Karianne'} id = { 4} email={'Julianne.OConner@kory.org'}  address:street={'Hoeger Mall'} address:suite={'Apt. 692'} address:city={'South Elvis'} address:zipcode={'53919-4257'} geo:lat={'29.4572'} geo:lng={'-164.2990'}/>
-            <User name = {'Chelsey Dietrich'} username = {'Kamren'} id = { 5} email={'Lucio_Hettinger@annie.ca'}  address:street={'Skiles Walks'} address:suite={'Suite 351'} address:city={'Roscoeview'} address:zipcode={'33263'} geo:lat={'-31.8129'} geo:lng={'62.5342'}/>
-            <User name = {'Mrs. Dennis Schulist'} username = {'Leopoldo_Corkery'} id = { 6} email={'Karley_Dach@jasper.info'}  address:street={'Norberto Crossing'} address:suite={'Apt. 950'} address:city={'South Christy'} address:zipcode={'23505-1337'} geo:lat={'-71.4197'} geo:lng={'71.7478'}/>
-            <User name = {'Kurtis Weissnat'} username = {'Elwyn.Skiles'} id = { 7} email={'Telly.Hoeger@billy.biz'}  address:street={'Rex Trail'} address:suite={'Suite 280'} address:city={'Howemouth'} address:zipcode={'58804-1099'} geo:lat={'24.8918'} geo:lng={'21.8984'}/>
-            <User name = {'Nicholas Runolfsdottir V'} username = {'Maxime_Nienow'} id = { 8} email={'Sherwood@rosamond.me'}  address:street={'Ellsworth Summit'} address:suite={'Suite 729'} address:city={'Aliyaview'} address:zipcode={'45169'} geo:lat={'-14.3990'} geo:lng={'-120.7677'}/>
-            <User name = {'Glenna Reichert'} username = {'Delphine'} id = { 9} email={'Chaim_McDermott@dana.io'}  address:street={'Dayna Park'} address:suite={'Suite 449'} address:city={'Bartholomebury'} address:zipcode={'76495-3109'} geo:lat={'24.6463'} geo:lng={'-168.8889'}/>
-            <User name = {'Clementina DuBuque'} username = {'Moriah.Stanton'} id = { 10} email={'Rey.Padberg@karina.biz'}  address:street={'Kattie Turnpike'} address:suite={'Suite 198'} address:city={'Lebsackbury'} address:zipcode={'31428-2261'} geo:lat={'-38.2386'} geo:lng={'57.2232'}/>
-            </div>
-    );
+export default function App() {
+    return (
+        <div>
+            App
+        </div>
+    )
 }
+
+
