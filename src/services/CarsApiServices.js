4192749b3b1035fd0bbd3a8b37ade77f2e41399a
olia0510 <olia.viderskaya@gmail.com>
2021-08-26 08:46:23 +0300

    Home work 4 change1

--- src/services/CarsApiServices.js
+++ src/services/CarsApiServices.js
@@ -1,12 +1,20 @@
-const saveCars = (car) => {
-fetch('http://91.201.233.14/api/v1/cars', {
-    method: 'POST',
-    body: JSON.stringify(car),
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-    },
-})
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+let url = 'https://jsonplaceholder.typicode.com/users'
+const getUsers = () => fetch(url)
+    .then(value => value.json())
+
+    export {getUsers};
+
+// const saveCars = (car) => {
+// fetch('http://91.201.233.14/api/v1/cars', {
+//     method: 'POST',
+//     body: JSON.stringify(car),
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//     },
+// })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// }
+// export {saveCars}
+export class saveCars {
 }
-export {saveCars}
